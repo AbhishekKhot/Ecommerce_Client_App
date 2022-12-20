a3f5d5937d26faa398d230e7483b5bc0c4ba396c
Abhishek Khot <khotabhishek469@gmail.com>
2022-12-20 20:06:02 +0530

    Address storing in preference data store

--- app/src/main/java/com/project/ecommerceapp/ui/AddressActivity.kt
+++ app/src/main/java/com/project/ecommerceapp/ui/AddressActivity.kt
@@ -1,13 +1,27 @@
 package com.project.ecommerceapp.ui
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Toast
 import com.project.ecommerceapp.databinding.ActivityAddressBinding
+import com.project.ecommerceapp.utils.Constants
+import com.project.ecommerceapp.utils.Constants.ADDRESS
+import com.project.ecommerceapp.utils.Constants.PIN_CODE
+import com.project.ecommerceapp.utils.Constants.SHARED_PREFERENCES_NAME
+import com.project.ecommerceapp.utils.Constants.USER_CITY_NAME
+import com.project.ecommerceapp.utils.Constants.USER_NAME
+import com.project.ecommerceapp.utils.Constants.USER_PHONE_NUMBER
+import com.project.ecommerceapp.utils.Constants.USER_STATE_NAME
 
 class AddressActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddressBinding
     private var totalAmount: Long? = null
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var editor: Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +31,71 @@ class AddressActivity : AppCompatActivity() {
         totalAmount = intent.getStringExtra("totalAmount")!!.toLong()
 
         Toast.makeText(this, totalAmount.toString(), Toast.LENGTH_SHORT).show()
+
+        sharedPref=getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+        editor=sharedPref.edit()
+
+        getAllValues()
+        binding.btnProceedToPay.setOnClickListener {
+            updateAllValues()
+            val intent = Intent(this, PaymentActivity::class.java)
+            intent.putExtra("totalAmount", totalAmount)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun updateAllValues() {
+        if (binding.etUserName.text.toString().isNotEmpty()) {
+            editor.putString(USER_NAME, binding.etUserName.text.toString())
+            editor.apply()
+        } else {
+            binding.etUserName.requestFocus()
+            binding.etUserName.error = "Empty"
+        }
+        if (binding.etUserNumber.text.toString().isNotEmpty()) {
+            editor.putString(USER_PHONE_NUMBER, binding.etUserNumber.text.toString())
+            editor.apply()
+        } else {
+            binding.etUserNumber.requestFocus()
+            binding.etUserNumber.error = "Empty"
+        }
+        if (binding.etCityName.text.toString().isNotEmpty()) {
+            editor.putString(USER_CITY_NAME, binding.etCityName.text.toString())
+            editor.apply()
+        } else {
+            binding.etCityName.requestFocus()
+            binding.etCityName.error = "Empty"
+        }
+        if (binding.etStateName.text.toString().isNotEmpty()) {
+            editor.putString(USER_STATE_NAME, binding.etStateName.text.toString())
+            editor.apply()
+        } else {
+            binding.etStateName.requestFocus()
+            binding.etStateName.error = "Empty"
+        }
+        if (binding.etPinCode.text.toString().isNotEmpty()) {
+            editor.putString(PIN_CODE, binding.etPinCode.text.toString())
+            editor.apply()
+        } else {
+            binding.etPinCode.requestFocus()
+            binding.etPinCode.error = "Empty"
+        }
+        if (binding.etUserAddress.text.toString().isNotEmpty()) {
+            editor.putString(ADDRESS, binding.etUserAddress.text.toString())
+            editor.apply()
+        } else {
+            binding.etUserAddress.requestFocus()
+            binding.etUserAddress.error = "Empty"
+        }
+    }
+
+    private fun getAllValues() {
+        binding.etUserName.setText(sharedPref.getString(USER_NAME, ""))
+        binding.etUserNumber.setText(sharedPref.getString(USER_PHONE_NUMBER, ""))
+        binding.etCityName.setText(sharedPref.getString(USER_CITY_NAME, ""))
+        binding.etStateName.setText(sharedPref.getString(USER_STATE_NAME, ""))
+        binding.etPinCode.setText(sharedPref.getString(PIN_CODE, ""))
+        binding.etUserAddress.setText(sharedPref.getString(ADDRESS, ""))
     }
 }
--- app/src/main/java/com/project/ecommerceapp/ui/PaymentActivity.kt
+++ app/src/main/java/com/project/ecommerceapp/ui/PaymentActivity.kt
@@ -1,19 +1,29 @@
 package com.project.ecommerceapp.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.project.ecommerceapp.databinding.ActivityPaymentBinding
+import com.project.ecommerceapp.utils.Constants
+import com.project.ecommerceapp.utils.Constants.SHARED_PREFERENCES_NAME
+import com.project.ecommerceapp.utils.Constants.USER_NAME
 
 class PaymentActivity : AppCompatActivity() {
     private lateinit var binding:ActivityPaymentBinding
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPref=getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
         val intent = Intent()
-        binding.text.text=intent.getStringExtra("totalAmount")
+        binding.text1.text=intent.getStringExtra("totalAmount")
+
+      Toast.makeText(this,sharedPref.getString(USER_NAME, "").toString(),Toast.LENGTH_LONG).show()
     }
 }
